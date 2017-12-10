f72a1f6f1d40886fda563da485f4dbec6ebce70c
Niraj Shah <niraj.ns.shah@gmail.com>
2017-12-10 17:17:50 -0500

    Julia asked her students to create some coding challenges.
    Julia asked her students to create some coding challenges. Write a query to print the hacker_id, name, and the total number of challenges created by each student. Sort your results by the total number of challenges in descending order. If more than one student created the same number of challenges, then sort the result by hacker_id. If more than one student created the same number of challenges and the count is less than the maximum number of challenges created, then exclude those students from the result.
    
    Input Format
    
    The following tables contain challenge data:
    
    Hackers: The hacker_id is the id of the hacker, and name is the name of the hacker. 
    
    Challenges: The challenge_id is the id of the challenge, and hacker_id is the id of the student who created the challenge.

---
+++ Challenges.sql
@@ -0,0 +1,42 @@
+
+set @max = (select max(cntChg)
+            from 
+            (select Challenges.hacker_id,Hackers.name,count(challenge_id) as cntChg
+                    from Hackers 
+                    inner join Challenges
+                    on Hackers.hacker_id = Challenges.hacker_id
+                    group by hacker_id,name
+                    order by cntChg desc,Challenges.hacker_id asc) as temp3);
+
+
+select temp3.hacker_id,temp3.name,temp3.cntChg
+from 
+        (select Challenges.hacker_id,Hackers.name,count(challenge_id) as cntChg
+                from Hackers 
+                inner join Challenges
+                on Hackers.hacker_id = Challenges.hacker_id
+                group by hacker_id,name
+                order by cntChg desc,Challenges.hacker_id asc) 
+                as temp3
+
+        inner join 
+
+
+        (select cntChg as chlg,count(cntChg) as times
+        from (
+                select Challenges.hacker_id,Hackers.name,count(challenge_id) as cntChg
+                from Hackers 
+                inner join Challenges
+                on Hackers.hacker_id = Challenges.hacker_id
+                group by hacker_id,name
+                order by cntChg desc,Challenges.hacker_id asc) 
+         as temp
+        group by cntChg
+        having times > 0
+        order by times desc) 
+        as temp2
+
+on temp3.cntChg = temp2.chlg
+where temp2.times = 1 or (temp2.times > 1 and temp3.cntChg >= @max)
+order by temp3.cntChg desc, temp3.hacker_id asc
+ 
